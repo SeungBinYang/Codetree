556c42914140fd0c27b164265a48af1986e2c2ea
Seungbin Yang 양승빈 <sbyang0731@gmail.com>
2025-08-20 18:59:20 +0900

    Title: 윤년인가, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250820/윤년인가/is-leap-year.py
+++ 250820/윤년인가/is-leap-year.py
@@ -1,6 +1,8 @@
 y = int(input())
 
 if (y % 4 == 0):
-    if (y % 100 == 0 and y % 400 != 0):
-        print("false")
-    else: print("true")
+    if not(y % 100 == 0 and y % 400 != 0):
+        print("true")
+    else: print("false")
+else:
+    print("false")
